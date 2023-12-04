0cb12426f68c2243f5da2c5716c8fa1b61bf48bb
TrojanerHD <github@trojaner.dev>
2023-12-04 06:48:26 +0100

    Simplify code of day 04

--- src/04/index.ts
+++ src/04/index.ts
@@ -8,7 +8,7 @@ export default class Day04 implements Day {
     let result1 = 0;
     let result2 = 0;
 
-    const duplicate: number[] = [];
+    const duplicate: number[] = new Array(lines.length).fill(0);
     for (const line of lines) {
       const [info, game] = line.split(': ');
       const card = Number(info.replace('Card ', ''));
@@ -22,20 +22,15 @@ export default class Day04 implements Day {
         .split(' ')
         .filter((mine) => mine !== '');
       let gameResult = 0;
-      let actualWon = 0;
-      for (const oneWinning of allWinning) {
-        if (allMine.includes(oneWinning)) {
-          if (!gameResult) gameResult = 1;
-          else gameResult *= 2;
-          ++actualWon;
-        }
-      }
-      for (let i = 0; i < actualWon; ++i)
-        duplicate[card + i + 1] = duplicate[card + i + 1]
-          ? duplicate[card + i + 1] + 1 * ((duplicate[card] ?? 0) + 1)
-          : 1 * ((duplicate[card] ?? 0) + 1);
+      for (const oneWinning of allWinning)
+        if (allMine.includes(oneWinning))
+          gameResult = gameResult === 0 ? 1 : gameResult * 2;
+
+      for (let i = 0; i < Math.log2(gameResult) + 1; ++i)
+        duplicate[card + i + 1] =
+          (duplicate[card + i + 1] ?? 0) + 1 * (duplicate[card] + 1);
       result1 += gameResult;
-      result2 += (duplicate[card] ?? 0) + 1;
+      result2 += duplicate[card] + 1;
     }
 
     return [result1, result2];
